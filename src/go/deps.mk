42ba86eac2e60b2d955d394d0cba5b941cbb6ae4
Kamil Samigullin <kamil@samigullin.info>
2020-04-07 23:12:42 +0300

    support go 1.11..13 and 1.14 and later

--- src/go/deps.mk
+++ src/go/deps.mk
@@ -13,6 +13,7 @@ deps-clean:
 .PHONY: deps-shake
 deps-shake:
 	@go mod tidy
+	@if [[ "`go env GOFLAGS`" =~ -mod=vendor ]]; then go mod vendor; fi
 
 .PHONY: module-deps
 module-deps:
@@ -26,8 +27,19 @@ update:
 		packages="`egg deps list`"; \
 	else \
 		packages="`go list -f $(selector) -m all`"; \
-	fi; go get -d -u $$packages
+	fi; \
+	if [[ "`go version`" == *1.1[1-3]* ]]; then \
+		go get -d -mod= -u $$packages; \
+	else \
+		go get -d -u $$packages; \
+	fi; \
+	if [[ "`go env GOFLAGS`" =~ -mod=vendor ]]; then go mod vendor; fi
 
 .PHONY: update-all
 update-all:
-	@go get -d -u ./...
+	@if [[ $(go version) == *1.1[1-3]* ]]; then \
+		go get -d -mod= -u ./...; \
+	else \
+		go get -d -u ./...; \
+	fi \
+	if [[ "`go env GOFLAGS`" =~ -mod=vendor ]]; then go mod vendor; fi
