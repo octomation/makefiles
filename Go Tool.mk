e3b788172d32583271efc4c14524a5282fca3238
Kamil Samigullin <kamil@samigullin.info>
2020-09-08 09:33:38 +0300

    fix #12: sync go makefiles

--- Go Tool.mk
+++ Go Tool.mk
@@ -2,7 +2,7 @@
 
 .DEFAULT_GOAL = test-with-coverage
 GIT_HOOKS     = post-merge pre-commit pre-push
-GO_VERSIONS   = 1.13 1.14
+GO_VERSIONS   = 1.14 1.15
 
 include src/common/env.mk
 include src/go/env.mk
