83cd86682d22a0bb5a465f6ad51dfcc45945069a
Kamil Samigullin <kamil@samigullin.info>
2020-10-06 00:11:54 +0000

    fix #5: add build-with-race and server-with-race

--- Go Service.mk
+++ Go Service.mk
@@ -22,6 +22,11 @@ server: MAIN   = ./cmd/server/main.go
 server: build
 .PHONY: server
 
+server-with-race: BINARY = $(BINPATH)/server-race
+server-with-race: MAIN   = ./cmd/server/main.go
+server-with-race: build-with-race
+.PHONY: server-with-race
+
 client: BINARY = $(BINPATH)/client
 client: MAIN   = ./cmd/client/main.go
 client: build
