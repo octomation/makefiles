15b6a5766c2337b9791dfaebc0f5f75fe29282cf
Kamil Samigullin <kamil@samigullin.info>
2021-02-23 23:29:34 +0300

    issue #49: handle go dist if goreleaser or godownloader are not present

--- src/go/dist.mk
+++ src/go/dist.mk
@@ -1,7 +1,15 @@
 dist-check:
-	$(AT) goreleaser --snapshot --skip-publish --rm-dist
+	$(AT) if command -v goreleaser > /dev/null; then \
+		goreleaser --snapshot --skip-publish --rm-dist; \
+	else \
+		echo have no goreleaser binary; \
+	fi
 .PHONY: dist-check
 
 dist-dump:
-	$(AT) godownloader .goreleaser.yml > bin/install
+	$(AT) if command -v godownloader > /dev/null; then \
+		godownloader .goreleaser.yml > bin/install; \
+	else \
+		echo have no godownloader binary; \
+	fi
 .PHONY: dist-dump
