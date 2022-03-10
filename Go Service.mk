4930d6cb3033c5667b0cd8f1846e35e0b45a7481
Kamil Samigullin <kamil@samigullin.info>
2022-03-10 09:48:30 +0300

    go: extend service makefile

--- Go Service.mk
+++ Go Service.mk
@@ -14,6 +14,8 @@ include src/go/deps.mk
 include src/go/dev.mk
 include src/go/test.unit.mk
 include src/go/test.integration.mk
+include src/go/test.fuzzing.mk
+include src/go/test.performance.mk
 include src/go/build.service.mk
 include src/go/dist.mk
 include src/go/tools.mk
@@ -21,44 +23,72 @@ include src/docker/go.mk
 
 export PATH := $(GOBIN):$(PATH)
 
-init: deps check hooks
-	$(AT) git config core.autocrlf input
-.PHONY: init
+setup: git-config git-hooks go-deps-fetch go-tools-install
+.PHONY: setup
 
-check: test lint
-.PHONY: check
-
-clean: deps-clean install-clean test-clean
+clean: go-deps-clean go-dist-clean go-install-clean go-test-clean go-fuzzing-test-clean
 .PHONY: clean
 
-deps: deps-fetch tools-install
-.PHONY: deps
+drop: clean git-clean git-rmdir
+.PHONY: drop
 
-docs: go-docs
-.PHONY: docs
-
-env: go-env build-env tools-env
+env: go-env go-build-env go-tools-env
 env:
 	@echo "PATH:        $(PATH)"
 .PHONY: env
 
+deps: go-deps-fetch
+.PHONY: deps
+
+tools: go-tools-install
+.PHONY: tools
+
+update:
+	@true # TODO:implement go-deps-update go-tools-update generate
+.PHONY: update
+
 format: go-fmt
 .PHONY: format
 
-generate: go-generate format
+generate: go-generate go-fmt
 .PHONY: generate
 
-refresh: deps-tidy update deps generate check build
-.PHONY: refresh
+docs:
+	@true # TODO:implement go-docs go-pkg
+.PHONY: docs
 
-tools: tools-install
-.PHONY: tools
+lint: go-lint
+.PHONY: lint
 
-update: deps-update tools-update
-.PHONY: update
+test: go-test
+.PHONY: test
+
+test-with-coverage: TIMEOUT=5s
+test-with-coverage: go-integration-test-with-coverage
+.PHONY: test-with-coverage
+
+check: lint test
+.PHONY: check
+
+fast-check: find-todos
+fast-check: go-check go-deps-check go-tools-check
+fast-check: go-deps-tidy go-tools-tidy go-generate git-check
+.PHONY: fast-check
+
+full-check: fast-check go-dist-check check
+.PHONY: full-check
+
+full-test: go-test go-fuzzing-test go-integration-test go-performance-test
+.PHONY: full-test
+
+client: go-client
+.PHONY: client
+
+server: go-server
+.PHONY: server
+
+install: go-install
+.PHONY: install
 
 verbose: make-verbose go-verbose
 .PHONY: verbose
-
-verify: deps-check generate check git-check
-.PHONY: verify
